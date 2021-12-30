d82a508b9b3e4cd85e0d3d9693137d9804b7794b
Matt Snow <mattsnow@gmail.com>
2021-12-30 03:44:24 +0000

    Improvements

--- main.tf
+++ main.tf
@@ -17,27 +17,52 @@ resource "libvirt_pool" "tfpool" {
   path = "/var/lib/libvirt/terraform/pool"
 }
 
-resource "libvirt_volume" "tfvm" {
+resource "libvirt_volume" "vm_volume" {
   count          = var.vm_id
-  name           = "tfvm${count.index}.qcow2"
+  name           = "${var.machines[count.index]["hostname"]}.qcow2"
   base_volume_id = libvirt_volume.focal-base.id
   size           = 30000000000 
   pool           = libvirt_pool.tfpool.name
 }
 
 resource "libvirt_cloudinit_disk" "commoninit" {
-  count = var.vm_id
+  count          = var.vm_id
   name           = "commoninit${count.index}.iso"
-  user_data      = templatefile("${path.module}/cloud_init.cfg", {instance = count.index, my_user_name = var.my_user_name, root_pw = var.root_pw, ssh_pub_key = var.ssh_pub_key, passwd_hash = var.passwd_hash} )
-  network_config = templatefile("${path.module}/network_config.cfg", {instance = count.index} )
+  user_data      = templatefile("${path.module}/cloud_init.cfg", { 
+                       hostname        = var.machines[count.index]["hostname"]
+                       my_user_name    = var.my_user_name, 
+                       root_pw         = var.root_pw, 
+                       ssh_pub_key     = var.ssh_pub_key, 
+                       hostname_prefix = var.hostname_prefix,
+                       passwd_hash     = var.passwd_hash
+                       } )
+  network_config = templatefile("${path.module}/network_config.cfg", {
+                       dns_domain_one  = var.dns_domain_one,
+                       dns_domain_two  = var.dns_domain_two,
+                       dns_ip_one      = var.dns_ip_one,
+                       dns_ip_two      = var.dns_ip_two,
+                       ethernet_if     = var.ethernet_if,
+                       ip_bitmask      = var.ip_bitmask,
+                       ip_gateway      = var.ip_gateway,
+                       ip_addr         = var.machines[count.index]["ip_addr"],
+                       } )
   pool           = libvirt_pool.tfpool.name
 }
 
-resource "libvirt_domain" "tfvm" {
+# Create the libvirt network.
+resource "libvirt_network" "vm_network" {
+  name           = "default"
+  mode           = "bridge"
+  bridge         = "virbr0"
+  autostart      = true
+}
+
+
+resource "libvirt_domain" "vm_domain" {
   count = var.vm_id
-  name   = "tfvm${count.index}.lt.co.slakin.net"
-  memory = "1024"
-  vcpu   = "1"
+  name   = "${var.machines[count.index]["hostname"]}"
+  memory = "2048"
+  vcpu   = "2"
   machine = "q35"
   cloudinit = libvirt_cloudinit_disk.commoninit[count.index].id
   
@@ -46,12 +71,13 @@ resource "libvirt_domain" "tfvm" {
   }
 
   disk {
-    volume_id = libvirt_volume.tfvm[count.index].id
+    volume_id = libvirt_volume.vm_volume[count.index].id
   }
 
   network_interface {
-    network_name    = "default"
-    bridge          = "eth0"
+    network_name  = "default"
+    bridge        = "eth0"
+    mac           = var.machines[count.index]["mac"]
   }
 
   console {
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 
 variable "vm_id" {
   type        = number
-  default     = 3
+  default     = 1
   description = "The number of KVM instances to create."
 }
 
@@ -20,3 +20,28 @@ variable "ssh_pub_key" {
 variable "my_user_name" {
   type = string
 }
+
+variable "dns_domain_name" {
+  type = string
+}
+
+variable "dns_domain_one" {}
+variable "dns_domain_two" {}
+variable "dns_ip_one" {}
+variable "dns_ip_two" {}
+variable "ip_bitmask" {}
+variable "ip_gateway" {}
+variable "ethernet_if" {}
+
+variable "hostname_prefix" {
+  type = string
+}
+
+variable "machines" {
+  type = list(object({
+    hostname = string
+    mac      = string
+    ip_addr  = string
+  }))
+}
+
